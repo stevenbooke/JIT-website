ef1505410d34daea0f13e43d08906deb6130f00a
stevenbooke <steviebeee55@gmail.com>
2024-04-14 10:37:30 -0700

    style(webpage): Leverage bootstrap5 and react-bootstrap npm package for cleaner code

--- src/App.js
+++ src/App.js
@@ -7,20 +7,130 @@ import About from './components/about/About';
 import Roadmap from './components/roadmap/Roadmap';
 import Buy from './components/buy/Buy';
 import Footer from './components/footer/Footer';
+import SvgJitWebsiteLayoutFrames1 from './components/svg/jit-website-layout-frames-objective-svg/JitWebsiteLayoutFrames1';
+import SvgJitWebsiteLayoutFrames2 from './components/svg/jit-website-layout-frames-objective-svg/JitWebsiteLayoutFrames2';
+import SvgJitWebsiteLayoutFrames3 from './components/svg/jit-website-layout-frames-objective-svg/JitWebsiteLayoutFrames3';
+import SvgJitWebsiteLayoutFrames4 from './components/svg/jit-website-layout-frames-objective-svg/JitWebsiteLayoutFrames4';
+import SvgJitWebsiteLayoutFrames5Wide from './components/svg/jit-website-layout-frames-objective-svg/JitWebsiteLayoutFrames5Wide';
+import SvgJitWebsiteLayoutFrames6 from './components/svg/jit-website-layout-frames-objective-svg/JitWebsiteLayoutFrames6';
+import { Row, Container, Col } from 'react-bootstrap';
 // import { Link, DirectLink, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
 
 
 const App = () => {
   return (
-      <main>
-        <Navigation />
-        <CriticalText/>
-        <About/>
-        <Roadmap/>
-        <Buy/>
-        <Footer/>
-      </main>
+    <div>
+      <section className="section1">
+        <div className="d-flex justify-content-center align-items-center">
+          <div className='d-flex flex-row align-items-center' style={{ position: 'absolute', zIndex: 1 }}>
+            {/* <p className="p-2 mt-auto mb-auto" >CA</p>
+            <p className="p-2 mt-auto mb-auto" >aaaaaaaaaaaaaaaaaa</p> */}
+            <h1 className="p-2 mt-auto mb-auto text-center fw-bold fs-1" >CA</h1>
+            <h1 className="p-2 mt-auto mb-auto text-center fw-light fs-2" >aaaaaaaaaaaaaaaaaa</h1>
+          </div>
+          <SvgJitWebsiteLayoutFrames1 style={{ position: 'relative', width: '100%', height: 'auto', top: 0, left: 0 }} />
+        </div>
+        {/* <Container fluid>
+          <Row>
+            <Col style={{ position: 'relative' }}>
+              <SvgJitWebsiteLayoutFrames1 />
+              <div className="d-flex justify-content-center" style={{ position: 'absolute', width: '100%', height: 'auto', top: 0, left: 0, zIndex: 1 }}> */}
+                {/* Your content for section 1 */}
+                {/* <h1>Section 1 Title</h1>
+                <p>This is some text for section 1.</p> */}
+                {/* <img src="image1.jpg" alt="Image 1" /> */}
+              {/* </div>
+            </Col>
+          </Row>
+        </Container> */}
+      </section>
+      <section className="section2">
+        <Container fluid>
+          <Row>
+            <Col style={{ position: 'relative' }}>
+              <SvgJitWebsiteLayoutFrames2 />
+              <div className="content" style={{ position: 'absolute', width: '100%', height: 'auto', top: 0, left: 0, zIndex: 1 }}>
+                {/* Your content for section 1 */}
+                <h1>Section 1 Title</h1>
+                <p>This is some text for section 1.</p>
+                <img src="image1.jpg" alt="Image 1" />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      <section className="section3">
+        <Container fluid>
+          <Row>
+            <Col>
+              <SvgJitWebsiteLayoutFrames3 />
+              {/* Your content for section 2 */}
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      <section className="section4">
+        <Container fluid>
+          <Row>
+            <Col>
+              <SvgJitWebsiteLayoutFrames4 />
+              {/* Your content for section 3 */}
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      <section className="section5">
+        <Container fluid>
+          <Row>
+            <Col>
+              <SvgJitWebsiteLayoutFrames5Wide />
+              {/* Your content for section 4 */}
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      <section className="section6">
+        <Container fluid>
+          <Row>
+            <Col>
+              <SvgJitWebsiteLayoutFrames6 />
+              {/* Your content for section 4 */}
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </div>
   );
 };
 
 export default App;
+
+
+
+
+
+// import React from 'react';
+// import "bootstrap/dist/css/bootstrap.min.css";
+// import "./App.css";
+// import Navigation from './components/nav/Navigation';
+// import CriticalText from './components/critical/CriticalText';
+// import About from './components/about/About';
+// import Roadmap from './components/roadmap/Roadmap';
+// import Buy from './components/buy/Buy';
+// import Footer from './components/footer/Footer';
+// // import { Link, DirectLink, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
+
+// const App = () => {
+//   return (
+//       <main>
+//         <Navigation />
+//         <CriticalText/>
+//         <About/>
+//         <Roadmap/>
+//         <Buy/>
+//         <Footer/>
+//       </main>
+//   );
+// };
+
+// export default App;
--- src/components/nav/Navigation.js
+++ src/components/nav/Navigation.js
@@ -1,40 +1,84 @@
-import React from 'react';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
 import "./Navigation.css";
 import SvgJitLogo from '../svg/jit-logos-objective-svg/JitLogo';
-// import SvgJitLogo from '../svg/jit-logos-adobe-png-to-svg/JitLogo';
 import navbarBackgroundsvg from "../../assets/svg/jit-website-layout-frames-objective-svg/jit-website-layout-frames-1.svg";
-// import navbarBackgroundsvg from "../../assets/svg/jit-website-layout-frames-adobe-png-to-svg/jit-website-layout-frames-1.svg";
-import SvgJitWebsiteLayoutFrames1 from '../svg/jit-website-layout-frames-adobe-png-to-svg/JitWebsiteLayoutFrames1';
-import { Link as LinkScroll, DirectLink, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
+import SvgJitWebsiteLayoutFrames1 from '../svg/jit-website-layout-frames-objective-svg/JitWebsiteLayoutFrames1';
+import { Link, DirectLink, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
 
 const options = {
     // your options here, for example:
     duration: 500,
     smooth: true,
-  };
+};
 
 const scrollToTop = () => {
     scroll.scrollToTop(options);
 };
 
-
 const Navigation = () => {
     return (
-        <header className="fixed-top site__header" style={{ backgroundSize: 'contain', backgroundImage: `url(${navbarBackgroundsvg})` }}>
-            <div className="d-flex align-items-center justify-content-between">
+        <Navbar bg="dark" expand="md lg" fixed="top" style={{ backgroundSize: 'contain', backgroundImage: `url(${navbarBackgroundsvg})` }}>
+            <Link className="me-auto" to="/">
                 <SvgJitLogo className="navbar-brand nav_ac" onClick={scrollToTop} />
-                <div className="d-flex align-items-center">
-                    <LinkScroll activeClass="active" className="nav_ac" to="critical" spy={true} smooth={true} duration={500} offset={-50}>CRITICAL TEXT</LinkScroll>
-                    <LinkScroll activeClass="active" className="nav_ac" to="about" spy={true} smooth={true} duration={500} offset={-50}>ABOUT</LinkScroll>
-                    <LinkScroll activeClass="active" className="nav_ac" to="roadmap" spy={true} smooth={true} duration={500} offset={-50}>ROADMAP</LinkScroll>
-                    <LinkScroll activeClass="active" className="nav_ac" to="buy" spy={true} smooth={true} duration={500} offset={-50}>HOW TO BUY</LinkScroll>
-                </div>
-            </div>
-        </header>
+            </Link>
+            <Navbar.Toggle aria-controls="basic-navbar-nav" />
+            <Navbar.Collapse id="basic-navbar-nav">
+                <Nav className="ms-auto">
+                    <Link activeClass="active" className="nav_ac" to="critical" spy={true} smooth={true} duration={500} offset={-50}>CRITICAL TEXT</Link>
+                    <Link activeClass="active" className="nav_ac" to="about" spy={true} smooth={true} duration={500} offset={-50}>ABOUT</Link>
+                    <Link activeClass="active" className="nav_ac" to="roadmap" spy={true} smooth={true} duration={500} offset={-50}>ROADMAP</Link>
+                    <Link activeClass="active" className="nav_ac" to="buy" spy={true} smooth={true} duration={500} offset={-50}>HOW TO BUY</Link>
+                </Nav>
+            </Navbar.Collapse>
+        </Navbar>
     );
 };
 
 export default Navigation;
+
+
+
+
+
+
+// import React from 'react';
+// import "./Navigation.css";
+// import SvgJitLogo from '../svg/jit-logos-objective-svg/JitLogo';
+// // import SvgJitLogo from '../svg/jit-logos-adobe-png-to-svg/JitLogo';
+// import navbarBackgroundsvg from "../../assets/svg/jit-website-layout-frames-objective-svg/jit-website-layout-frames-1.svg";
+// // import navbarBackgroundsvg from "../../assets/svg/jit-website-layout-frames-adobe-png-to-svg/jit-website-layout-frames-1.svg";
+// import SvgJitWebsiteLayoutFrames1 from '../svg/jit-website-layout-frames-adobe-png-to-svg/JitWebsiteLayoutFrames1';
+// import { Link as LinkScroll, DirectLink, Element, Events, animateScroll as scroll, scrollSpy, scroller } from 'react-scroll';
+
+// const options = {
+//     // your options here, for example:
+//     duration: 500,
+//     smooth: true,
+//   };
+
+// const scrollToTop = () => {
+//     scroll.scrollToTop(options);
+// };
+
+
+// const Navigation = () => {
+//     return (
+//         <header className="fixed-top site__header" style={{ backgroundSize: 'contain', backgroundImage: `url(${navbarBackgroundsvg})` }}>
+//             <div className="d-flex align-items-center justify-content-between">
+//                 <SvgJitLogo className="navbar-brand nav_ac" onClick={scrollToTop} />
+//                 <div className="d-flex align-items-center">
+//                     <LinkScroll activeClass="active" className="nav_ac" to="critical" spy={true} smooth={true} duration={500} offset={-50}>CRITICAL TEXT</LinkScroll>
+//                     <LinkScroll activeClass="active" className="nav_ac" to="about" spy={true} smooth={true} duration={500} offset={-50}>ABOUT</LinkScroll>
+//                     <LinkScroll activeClass="active" className="nav_ac" to="roadmap" spy={true} smooth={true} duration={500} offset={-50}>ROADMAP</LinkScroll>
+//                     <LinkScroll activeClass="active" className="nav_ac" to="buy" spy={true} smooth={true} duration={500} offset={-50}>HOW TO BUY</LinkScroll>
+//                 </div>
+//             </div>
+//         </header>
+//     );
+// };
+
+// export default Navigation;
 
 
 
